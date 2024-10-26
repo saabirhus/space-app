0274568fb6ba67d64be37d4c958cc153093d91f1
Saabir Hussein <saabirhus2003@gmail.com>
2024-10-26 18:01:58 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is my first React App</h1>
+      <p>lorem ipsum dolor sit amet</p>
+    </div>
+  );
+}
+
+export default App;
